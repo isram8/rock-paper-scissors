aa17ad509bc191290ca34148b68e5aef2b61d163
Isram8 <israfilrules@live.com>
2021-07-31 19:10:07 +0300

    Added AI that chooses between rock, paper, and scissors at random. Added a prompt asking the player to choose rock, paper, or scissors.

--- scripts/rock_paper_scissors.js
+++ scripts/rock_paper_scissors.js
@@ -1,6 +1,55 @@
-function computerPlay() {
+function computerPlay() 
+{
     const computerChoice = ["Rock", "Paper", "Scissors"]
     return computerChoice[Math.floor(Math.random() * 3)];
 }
 
-alert(computerPlay());
+function playerPlay() 
+{
+    let playerChoice;
+    do
+    {
+        playerChoice = prompt("Rock, Paper or Scissors?")
+    }
+    while (playerChoice != "Rock" && playerChoice != "Paper" && playerChoice != "Scissors") 
+
+        return playerChoice;
+
+}
+
+let computerChoice = computerPlay();
+let playerChoice = playerPlay();
+
+function game(computerChoice, playerChoice) 
+{
+    if (computerChoice === "Rock" && playerChoice === "Paper")
+    {
+        alert(`You win! ${playerChoice} beats ${computerChoice}`);
+    }
+    else if (computerChoice === "Paper" && playerChoice === "Scissors")
+    {
+        alert(`You win! ${playerChoice} beats ${computerChoice}`);
+    }
+    else if (computerChoice === "Scissors" && playerChoice === "Rock")
+    {
+        alert(`You win! ${playerChoice} beats ${computerChoice}`);
+    }
+    else if (computerChoice === "Paper" && playerChoice === "Rock")
+    {
+        alert(`You lose! ${computerChoice} beats ${playerChoice}`);
+    }
+    else if (computerChoice === "Rock" && playerChoice === "Scissors")
+    {
+        alert(`You lose! ${computerChoice} beats ${playerChoice}`);
+    } 
+    else if (computerChoice === "Scissors" && playerChoice === "Paper")
+    {
+        alert(`You lose! ${computerChoice} beats ${playerChoice}`);
+    }
+    else 
+    {
+        alert(`It's a tie! Both players chose ${computerChoice}`);
+    }
+}
+
+game(computerChoice, playerChoice);
